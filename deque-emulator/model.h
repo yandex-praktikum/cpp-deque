974ec8bbdf5f46e685a929c07bfeb3c38651476b
DiSayrus <dianasayrus@yandex.ru>
2026-09-02 07:46:18 +0300

    Update model.h

--- deque-emulator/model.h
+++ deque-emulator/model.h
@@ -1,3 +1,13 @@
 #pragma once
 
-// В качестве заготовки возьмите последний эмулятор вектора.
+#include <deque>
+#include <string>
+#include <random>
+
+struct Model {
+    using container = std::deque<std::string>;
+    container items;
+    container::iterator iterator = items.begin();
+
+    std::mt19937 random_gen;
+};
